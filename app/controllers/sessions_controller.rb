7eb76276327689a20b9b0e7a719bb7eb36142ca7
Megan Talbot <meganftalbot@gmail.com>
2016-12-06 13:17:49 -0700

    Intial apicurious commit, simple bootstrap

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def create
+   @response =
+   Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV["github_client_id"]}&client_secret=#{ENV["github_client_secret"]}&code=#{params["code"]}")
+   token = @response.body.split(/\W+/)[1]
+   oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+   auth = JSON.parse(oauth_response.body)
+
+   user                 = User.find_or_create_by(uid: auth["id"])
+   user.username        = auth["login"]
+   user.uid             = auth["id"]
+   user.token           = token
+   user.profile_picture = auth["avatar_url"]
+   user.name            = auth["name"]
+   user.joined_date     = auth["created_at"]
+   user.location     = auth["location"]
+   user.save
+
+   session[:user_id] = user.id
+   redirect_to dashboard_index_path
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get '/auth/github/callback', to: 'sessions#create'
+  resources :dashboard, only: [:index]
+  delete '/logout', to: 'sessions#destroy', as: :logout
+end
